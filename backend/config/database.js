7bf13b9f7f6c6e8344721b374381e844eee467e6
Nmcleon <nmcleon10@gmail.com>
2024-02-02 13:13:09 +0200

    set up basic project
    Mongoose 6.0 documentation, the options useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Therefore, emoved from code

---
+++ backend/config/database.js
@@ -0,0 +1,31 @@
+/**NO LONGER SUPPORTED IN VERSIONS ABV 6.0
+ const mongoose = require("mongoose");
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false, 
+      createIndexes: false, 
+    });
+
+    console.log(`MongoDB connected to HOST: ${mongoose.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+  }
+};*/
+
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI).then(() => {
+        console.log(`MongoDB connected to HOST: ${mongoose.connection.host}`);
+    }).catch(error => {
+        console.error(`MongoDB connection error: ${error.message}`);
+    });
+};
+
+module.exports = connectDatabase;
+
+
+module.exports = connectDatabase;
