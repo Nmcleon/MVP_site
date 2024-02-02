6c6a2d6cf248222a8ac62ac38d76d17e34749eae
Nmcleon <nmcleon10@gmail.com>
2024-02-03 00:05:12 +0200

    add delete

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
 
 router.route('/products').get(getProducts);
+router.route('/product/new').post(newProduct);
+
+
 router.route('/admin/products/:id').get(getSingleProduct);
 router.route('/admin/products/:id').put(updateProduct);
-router.route('/product/new').post(newProduct);
+router.route('/admin/products/:id').delete(deleteProduct);
+
 
 module.exports = router;
