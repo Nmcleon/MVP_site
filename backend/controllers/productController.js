6e84d601ace52c0ee6c4c983175bbbfc53f34706
Nmcleon <nmcleon10@gmail.com>
2024-02-02 23:46:35 +0200

    update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,7 +24,7 @@ exports.getProducts = async (req, res, next) => {
 	})
 }
 
-//Get single Product => /api/vi/product/:id
+//Get single Product => /api/vi/products/:id
 
 exports.getSingleProduct = async (req, res, next) => {
 
@@ -39,6 +39,30 @@ exports.getSingleProduct = async (req, res, next) => {
 
 	res.status(200).json({
 		success:true,
+		data: product
+	})
+}
+
+// Update product => /api/v1/admin/products/:id
+exports.updateProduct = async (req, res, next) => {
+
+	let product = await Product.findById(req.params.id);
+
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+
+	product = await Product.findByIdAndUpdate(req.params.id,  req.body, { 
+		new: true, // return the updated data
+		runValidators: true,
+		useFindAndModify: false // To avoid error when using findOneAndUpdate()
+	});
+
+	res.status(200).json({
+		success: true,
 		data: product
 	})
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
 
 
 router.route('/products').get(getProducts);
-router.route('/products/:id').get(getSingleProduct);
+router.route('/admin/products/:id').get(getSingleProduct);
+router.route('/admin/products/:id').put(updateProduct);
 router.route('/product/new').post(newProduct);
 
 module.exports = router;
