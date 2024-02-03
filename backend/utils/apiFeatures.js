0b581302a9b2305ead4c8d3327ccb2f009ef7059
Nmcleon <nmcleon10@gmail.com>
2024-02-03 15:57:28 +0200

    add extra layers of filtering

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -20,21 +20,46 @@ class APIFeatures {
 	}
   
 	filter() {
-	  const queryCopy = { ...this.queryStr };
-	  // Remove irrelevant fields
-	  const removeFields = ['keyword', 'limit', 'page']; // 'Limit' should be 'limit'
-	  removeFields.forEach((el) => delete queryCopy[el]);
-
-	  console.log(queryCopy);
-
-	  //advanced Rating
-	  let queryStr = JSON.stringify(queryCopy);
-	  queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+		const queryCopy = { ...this.queryStr };
+		// Remove irrelevant fields
+		const removeFields = ['keyword', 'limit', 'page'];
+		removeFields.forEach((el) => delete queryCopy[el]);
 	  
-	  console.log(queryStr);
-	  this.query = this.query.find(JSON.parse(queryStr)); // Use this.query instead of this.queryStr
-	  return this;
-	}
+		// Filter by keyword
+		if ('keyword' in queryCopy) {
+		  queryCopy.name = {
+			$regex: queryCopy.keyword,
+			$options: 'i',
+		  };
+		  delete queryCopy.keyword;
+		}
+	  
+		// Advanced Price Filtering
+		if ('maxPrice' in queryCopy) {
+		  queryCopy.price = { $lte: queryCopy.maxPrice };
+		  delete queryCopy.maxPrice;
+		}
+	  
+		if ('minPrice' in queryCopy) {
+		  queryCopy.price = { ...queryCopy.price, $gte: queryCopy.minPrice };
+		  delete queryCopy.minPrice;
+		}
+	  
+		// Filter by seller
+		if ('seller' in queryCopy) {
+		  queryCopy.seller = queryCopy.seller;
+		}
+	  
+		console.log(queryCopy);
+	  
+		// Advanced Rating
+		let queryStr = JSON.stringify(queryCopy);
+		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+	  
+		this.query = this.query.find(JSON.parse(queryStr));
+		return this;
+	  }
+	  
   }
   
   module.exports = APIFeatures;
