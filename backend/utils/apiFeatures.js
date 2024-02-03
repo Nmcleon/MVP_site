f238b8fd395f704bd6d978fee39a9313452f505a
Nmcleon <nmcleon10@gmail.com>
2024-02-03 15:49:57 +0200

    advance filter product(pice)

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -11,13 +11,31 @@ class APIFeatures {
 			  $regex: this.queryStr.keyword,
 			  $options: 'i',
 			},
-		  } : {}
+		  }
+		: {};
   
 	  console.log(keyword);
 	  this.query = this.query.find({ ...keyword });
 	  return this;
 	}
-  }
+  
+	filter() {
+	  const queryCopy = { ...this.queryStr };
+	  // Remove irrelevant fields
+	  const removeFields = ['keyword', 'limit', 'page']; // 'Limit' should be 'limit'
+	  removeFields.forEach((el) => delete queryCopy[el]);
 
-  module.exports = APIFeatures
+	  console.log(queryCopy);
+
+	  //advanced Rating
+	  let queryStr = JSON.stringify(queryCopy);
+	  queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+	  
+	  console.log(queryStr);
+	  this.query = this.query.find(JSON.parse(queryStr)); // Use this.query instead of this.queryStr
+	  return this;
+	}
+  }
+  
+  module.exports = APIFeatures;
   
