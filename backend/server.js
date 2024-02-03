6071ca9d80f710ceb770c35efeb2ac3e28fcf1e8
Nmcleon <nmcleon10@gmail.com>
2024-02-03 14:02:51 +0200

    handle promise rejection

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ dotenv.config({ path: 'backend/config/config.env' })
 //Connect to DB
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-	console.log(`Server running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
-
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })
+
+// Handle Unhandled Promise rejection
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting down server due to Unhandled Promise Rejection");
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
