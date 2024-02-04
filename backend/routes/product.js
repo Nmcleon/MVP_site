fff9c1808cec87d197c035459b3e9aa5594c9501
Nmcleon <nmcleon10@gmail.com>
2024-02-05 00:24:23 +0200

    add authentification

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,11 +5,11 @@ const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct 
 const{ isAuthenticatedUser } = require('../middlewares/auth');
 // Products routes
 router.route('/products').get(isAuthenticatedUser, getProducts);
-router.route('/product/new').post(newProduct);
+router.route('/product/new').post(isAuthenticatedUser, newProduct);
 
 // Admin routes
 router.route('/admin/products/:id').get(getSingleProduct);
-router.route('/admin/products/:id').put(updateProduct);
-router.route('/admin/products/:id').delete(deleteProduct);
+router.route('/admin/products/:id').put(isAuthenticatedUser, updateProduct);
+router.route('/admin/products/:id').delete(isAuthenticatedUser, deleteProduct);
 
 module.exports = router;
