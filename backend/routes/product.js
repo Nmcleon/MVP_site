d267a6ba9db3936cf681fa4ef50eb18ecaa3450c
Nmcleon <nmcleon10@gmail.com>
2024-02-04 23:18:45 +0200

    protecting routes frm unauth users

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-
+const{ isAuthenticatedUser } = require('../middlewares/auth');
 // Products routes
-router.route('/products').get(getProducts);
+router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/new').post(newProduct);
 
 // Admin routes
