51c5929d434be5f66201eb48bfa67822f0102a24
Nmcleon <nmcleon10@gmail.com>
2024-02-04 23:27:35 +0200

    implement logout

--- backend/controllers/authControler.js
+++ backend/controllers/authControler.js
@@ -58,3 +58,17 @@ exports.loginUser = catchAsyncError( async(req, res, next) => {
 		token
 	})*/
 })
+
+//logout user => /api/vi/logout
+exports.logout = catchAsyncError(async(req, res, next) => {
+	res.cookie('token', null, {
+		expires: new Date(Date.now()),
+		httpOnly: true
+
+	})
+
+	res.status(200).json({
+		success: true,
+		message: 'Logged out'
+	})
+}) 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser } = require('../controllers/authControler');
+const { registerUser, loginUser, logout } = require('../controllers/authControler');
 
 router.route('/register').post(registerUser);
 // router.route('/login')
 router.route('/login').post(loginUser);
+// logout
+router.route('/logout').get(logout);
+
 
 module.exports = router;
