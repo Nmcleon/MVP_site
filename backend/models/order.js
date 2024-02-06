fff89c25e3a2ec263f59b5d804d9a269437f3b44
Nmcleon <nmcleon10@gmail.com>
2024-02-06 11:39:54 +0200

    create order model

---
+++ backend/models/order.js
@@ -0,0 +1,108 @@
+const { default: mongoose } = require('mongoose')
+const mangoose = require('mongoose')
+
+const orderSchema = mangoose.Schema({
+	shippingInfo: {
+		address: {
+			type: String,
+			required: true
+		},
+		address :{
+			type: String,
+			required: true
+		},
+		city :{
+			type: String,
+			required: true
+		},
+		phoneNo :{
+			type: String,
+			required: true
+		},
+		postalCode :{
+			type: String,
+			required: true
+		},
+		country :{
+			type: String,
+			required: true
+		}
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User'
+	},
+	orderItems : [
+		{
+			name: {
+				type: String,
+				required: true
+			},
+			quantity : {
+				type: Number,
+				required: true
+			},
+			image: {
+				type : String,
+				requires: true
+			},
+			price: {
+				type: Number,
+				required: true
+			},
+			product : {
+				type: mongoose.Schema.type.ObjectId,
+				required: true,
+				ref: 'Product'
+			}
+		}
+		
+	],
+	paymentInfo: {
+		id: {
+			type : String
+		},
+		status : {
+			type : String
+		}
+	},
+	paidAt : {
+		type : Date
+	},
+	itemsPrice : {
+		type: Number,
+		required: true,
+		default: 0.0
+	},
+	taxPrice : {
+		type: Number,
+		required: true,
+		default: 0.0
+	},
+	shippingPrice : {
+		type: Number,
+		required: true,
+		default: 0.0
+	},
+	totalPrice : {
+		type: Number,
+		required: true,
+		default: 0.0
+	},
+	orderStatus : {
+		type : String,
+		required: true,
+		default: 'Procressing'
+	},
+	deliveredAt : {
+		type: Date,
+	},
+	createdAt : {
+		type : Date,
+		default : Date.now
+	}
+
+})
+
+model.exports = mongoose.model('Order', orderSchema)
