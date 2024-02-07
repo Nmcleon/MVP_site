d71f9ca68e0dd4ed8a9784c737547a0bd6dbf2a7
Nmcleon <nmcleon10@gmail.com>
2024-02-08 00:52:13 +0200

    implement redux and add store
    redux will be used to manage store.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='container container-fluid'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
